9531da482dec42c50baecfb7c3ecc850719eb6da
Felipe <fpachecomanoel@uol.com.br>
2025-08-03 14:22:14 -0300

    fixed coordinates in entity matrix generation and now level matrix also is edited when entity matrix changes

--- src/controller/Player.gd
+++ src/controller/Player.gd
@@ -2,7 +2,7 @@ class_name Player extends Entity
 
 func move(direction):
 	var target=Globals.game_controller.get_element(position+direction)
-	var target_floor=Globals.game_controller.get_element(position+direction+Vector3.DOWN)
+	var target_floor=Globals.game_controller.get_element(position+direction+Vector3.FORWARD)
 	if target.can_move_into(self,direction) and target_floor.can_move_over(self,direction):
 		target.move_into(self,direction)
 		target_floor.move_over(self,direction)
--- src/controller/game_controller.gd
+++ src/controller/game_controller.gd
@@ -9,7 +9,7 @@ var level_matrix
 var matrix_size
 var entity_matrix 
 var player 
-var can_move = true
+var can_move = false
 func _ready() -> void:
 	Globals.game_controller=self
 	start_level()
@@ -17,37 +17,42 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if can_move:
 		player.move(loop_array[loop_position])
-		var a = Vector3.FORWARD
 		loop_position=(loop_position+1)%loop_size
 		can_move = false
 		%Timer.start(2)
-		print(entity_matrix)
 		
 func start_level():
 	level_matrix=level.static_matrix
 	matrix_size=level.static_matrix_size
-	entity_matrix=level_matrix.duplicate()
+	entity_matrix=level_matrix.duplicate(true)
 	for x in range(matrix_size.x):
 		for z in range(matrix_size.z):
 			for y in range(matrix_size.y):
 				if level_matrix[x][z][y]=="ground":
 					entity_matrix[x][z][y]=Ground.new()
-					entity_matrix[x][z][y].position=Vector3(x,y,z)
+					entity_matrix[x][z][y].position=Vector3(x,z,y)
 				elif level_matrix[x][z][y]=="air":
 					entity_matrix[x][z][y]=Air.new()
-					entity_matrix[x][z][y].position=Vector3(x,y,z)
+					entity_matrix[x][z][y].position=Vector3(x,z,y)
 				elif level_matrix[x][z][y]=="player":
 					player=Player.new()
 					entity_matrix[x][z][y]=player
-					entity_matrix[x][z][y].position=Vector3(x,y,z)
+					Globals.player=player
+					entity_matrix[x][z][y].position=Vector3(x,z,y)
 					
+	can_move=true
 
 func get_element(position):
 	return entity_matrix[position[0]][position[1]][position[2]]
 	
 func set_element(position,value):
 	entity_matrix[position[0]][position[1]][position[2]]=value
-
+	var string
+	if value is Player:
+		string="player"
+	else: 
+		string="air"
+	level_matrix[position[0] as int][position[1] as int][position[2]as int]=string
 
 func _on_timer_timeout() -> void:
 	can_move = true
